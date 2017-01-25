c72a203c8fbbea13d3d3ee7465152420c71590d5
John He <hejohn@live.ca>
2017-01-24 17:14:28 -0800

    remove unused code
    remove unused code

--- p1.ino
+++ p1.ino
@@ -4,11 +4,15 @@
 
 #define JOYSTICK_X_PIN       A0
 #define JOYSTICK_Y_PIN       A1
-#define JOYSTICK_PIN_BUTTON  44
-#define LASER_PIN            10
 #define PHOTORESISTOR_PIN    A5
+
+//PWM 
+#define TILT_SERVO_PIN        8
 #define PAN_SERVO_PIN         9
-//#define TILT_SERVO_PIN     10
+#define LASER_PIN            10
+
+//Digital
+#define JOYSTICK_PIN_BUTTON  44
 
 // set the LCD address to 0x27 for a 16 chars 2 line display
 // A FEW use address 0x3F
@@ -21,43 +25,11 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 Servo panServo; 
 int panAngle = 90;
 
+//TILT SERVO Motor (Y)
+Servo tiltServo;
+int tiltAngle = 90;
+
 const int maxStepAngle = 5;
-
-// TILE SERVO MOTOR
-//Servo tiltServo;
-//int tiltAngle = 90;
-
-void lcdTest()
-{
-  // ------- Quick 3 blinks of backlight  -------------
-  for(int i = 0; i < 3; i++)
-  {
-    lcd.backlight();
-    delay(250);
-    lcd.noBacklight();
-    delay(250);
-  }
-  lcd.backlight(); // finish with backlight on  
-
-
-//-------- Write characters on the display ------------------
-// NOTE: Cursor Position: (CHAR, LINE) start at 0  
-  lcd.setCursor(0,0); //Start at character 4 on line 0
-  lcd.print("Hello, world!");
-  delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print("HI!YourDuino.com");
-  delay(8000);  
-
-// Wait and then tell user they can start the Serial Monitor and type in characters to
-// Display. (Set Serial Monitor option to "No Line Ending")
-  lcd.clear();
-  lcd.setCursor(0,0); //Start at character 0 on line 0
-  lcd.print("Use Serial Mon");
-  lcd.setCursor(0,1);
-  lcd.print("Type to display");  
-
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -67,7 +39,9 @@ void setup() {
 
   lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
   
-  panServo.attach(PAN_SERVO_PIN);
+  panServo.attach(PAN_SERVO_PIN);     // Pan servo
+  tiltServo.attach(TILT_SERVO_PIN);   // Tilt Servo
+  
   centerServoPosition();
   delay(500);
 }
@@ -92,8 +66,6 @@ void laserState(int inputValue)
 
 
 int getJoyStickPercentage(int x){
-  const int max_X = 1015;
-  const int max_Y = 1015;
   int mapped  = map(x, 0 , 1023, -maxStepAngle, maxStepAngle );
 //  Serial.print(mapped);
   if(  -2 <= mapped && mapped <= 2 ){
@@ -109,23 +81,27 @@ int getJoyStickPercentage(int x){
 
 void centerServoPosition(){
   panServo.write(90);
+  tiltServo.write(90);
 }
 
-void servoMovement(Servo theServo, int angle){
-   int maxAngle = 135;
-   int minAngle = 45;
-   panAngle += angle;
-   if( panAngle >= maxAngle ){
-      panAngle = maxAngle;
-   }else if( panAngle <= minAngle){
-      panAngle = minAngle;
+void servoMovement(Servo theServo, int angle, int* servoAngle, int minAngle, int maxAngle){
+//    int temp = *servoAngle; 
+    
+   *servoAngle += angle;
+   if( *servoAngle >= maxAngle ){
+      *servoAngle = maxAngle;
+   }else if( *servoAngle <= minAngle){
+      *servoAngle = minAngle;
    }
   // in steps of 1 degree
-  theServo.write(panAngle);        // tell servo to go to position in variable 'pos'
+  theServo.write(*servoAngle);        // tell servo to go to position in variable 'pos'
 
-  lcd.setCursor(11,1);
+  lcd.setCursor(8,1);
   lcd.print(panAngle);
 
+  lcd.setCursor(12,1);
+  lcd.print(tiltAngle);
+  
 }
 
 void parseAndExecuteJoystickInput()
@@ -133,6 +109,8 @@ void parseAndExecuteJoystickInput()
   int joystickX = analogRead(JOYSTICK_X_PIN);
   int joystickY = analogRead(JOYSTICK_Y_PIN);
   int joystickButton = digitalRead(JOYSTICK_PIN_BUTTON);
+  byte inArray[9]; //request code for real-time data
+
 //  // print out the value you read:
 //  Serial.print(joystickX);
 //  Serial.print(", ");
@@ -149,15 +127,15 @@ void parseAndExecuteJoystickInput()
   lcd.print(joystickY);
 
   laserState(joystickButton);
-
-//  getJoyStickPercentage(joystickX);
   
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   
   lcd.clear();
+  
   int joystickX = analogRead(JOYSTICK_X_PIN);
   int joystickY = analogRead(JOYSTICK_Y_PIN);
   int joystickButton = digitalRead(JOYSTICK_PIN_BUTTON);
@@ -175,10 +153,14 @@ void loop() {
 
 // ------ Servo Motor data -------
 
-  int stepAngleX = getJoyStickPercentage(joystickX);
+  int stepAngleX = getJoyStickPercentage(joystickX);     // the step angle that needs to be increased from joystick X
 
-  servoMovement( panServo, stepAngleX);  
-    
-  delay(50);        // delay in between reads for stability
+  int stepAngleY = getJoyStickPercentage(joystickY);    // the step angle that needs to be increased from joystick Y
+
+  servoMovement( panServo, stepAngleX, &panAngle,10, 170);  
+ 
+  servoMovement( tiltServo, stepAngleY, &tiltAngle,60, 135 );  
+
+  delay(25);        // delay in between reads for stability
 
 }
