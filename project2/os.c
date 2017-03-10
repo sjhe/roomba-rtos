ac0741a01dac1d771e3db48b55a66d77afb63677
John He <hejohn@live.ca>
2017-03-10 15:34:18 -0800

    organizing functions

--- project2/os.c
+++ project2/os.c
@@ -13,6 +13,21 @@ extern void a_main();
 static queue_t system_queue;
 static queue_t rr_queue;
 
+
+/* Kernel functions */
+static void kernel_handle_request(void);
+static void Next_Kernel_Request(void); 
+static void Dispatch(void);
+
+/* Context Switching*/
+extern void Exit_Kernel();    /* this is the same as CSwitch() */
+extern void CSwitch();
+extern void Enter_Kernel();
+
+
+/* Task management  */
+static void Kernel_Create_Task( voidfuncptr f);
+
 /*
  * FUNCTIONS
  */
@@ -72,8 +87,6 @@ static void idle (void)
   * again, but Cp is not running any more. 
   * (See file "switch.S" for details.)
   */
-extern void CSwitch();
-extern void Exit_Kernel();    /* this is the same as CSwitch() */
 
 /* Prototype */
 void Task_Terminate(void);
@@ -89,30 +102,10 @@ void Task_Terminate(void);
   *     as an external function call, it must be done explicitly. When Enter_Kernel()
   *     returns, then interrupts will be re-enabled by Enter_Kernel().
   */ 
-extern void Enter_Kernel();
 
 #define Disable_Interrupt()		asm volatile ("cli"::)
 #define Enable_Interrupt()		asm volatile ("sei"::)
   
-
-// void setup () {
-//   init_LED_ON_BOARD();
-//   init_LED_PING();
-//   init_LED_ISR();
-  //Clear timer config.
-  // TCCR3A = 0;      // Timer 3 A
-  // TCCR3B = 0;      // Timer 3 B
-  // //Set to CTC (mode 4)
-  // TCCR3B |= (1<<WGM32);
-  // //Set prescaller to 256
-  // TCCR3B |= (1<<CS32);
-  // //Set TOP value (1 milisecond)
-  // OCR3A = 62500;
-  // //Enable interupt A for timer 3.
-  // TIMSK3 |= (1<<OCIE3A);
-  // //Set timer to 0 (optional here).
-  // TCNT3 = 0;
-// }
 /**
  * When creating a new task, it is important to initialize its stack just like
  * it has called "Enter_Kernel()"; so that when we switch to it later, we
@@ -127,35 +120,36 @@ void Kernel_Create_Task_At( PD *p, voidfuncptr f )
 
   /*----BEGIN of NEW CODE----*/
   //Initialize the workspace (i.e., stack) and PD here!
-
   //Clear the contents of the workspace
   memset(&(p->workSpace),0,WORKSPACE);
 
-   //Notice that we are placing the address (16-bit) of the functions
-   //onto the stack in reverse byte order (least significant first, followed
-   //by most significant).  This is because the "return" assembly instructions 
-   //(rtn and rti) pop addresses off in BIG ENDIAN (most sig. first, least sig. 
-   //second), even though the AT90 is LITTLE ENDIAN machine.
+  //Notice that we are placing the address (16-bit) of the functions
+  //onto the stack in reverse byte order (least significant first, followed
+  //by most significant).  This is because the "return" assembly instructions 
+  //(rtn and rti) pop addresses off in BIG ENDIAN (most sig. first, least sig. 
+  //second), even though the AT90 is LITTLE ENDIAN machine.
 
-   //Store terminate at the bottom of stack to protect against stack underrun.
-   *(unsigned char *)sp-- = ((unsigned int)Task_Terminate) & 0xff;
-   *(unsigned char *)sp-- = (((unsigned int)Task_Terminate) >> 8) & 0xff;
+  //Store terminate at the bottom of stack to protect against stack underrun.
+  *(unsigned char *)sp-- = ((unsigned int)Task_Terminate) & 0xff;
+  *(unsigned char *)sp-- = (((unsigned int)Task_Terminate) >> 8) & 0xff;
 
-   //Place return address of function at bottom of stack
-   *(unsigned char *)sp-- = ((unsigned int)f) & 0xff;
-   *(unsigned char *)sp-- = (((unsigned int)f) >> 8) & 0xff;
-   *(unsigned char *)sp-- = 0x00;
-      //Place stack pointer at top of stack
-   sp = sp - 34;      
-   p->sp = sp;    /* stack pointer into the "workSpace" */
+  //Place return address of function at bottom of stack
+  *(unsigned char *)sp-- = ((unsigned int)f) & 0xff;
+  *(unsigned char *)sp-- = (((unsigned int)f) >> 8) & 0xff;
+  *(unsigned char *)sp-- = 0x00;
+    //Place stack pointer at top of stack
+  sp = sp - 34;      
+  p->sp = sp;    /* stack pointer into the "workSpace" */
 
-   p->code = f;		/* function to be executed as a task */
-   p->request = NONE;
+  p->code = f;		/* function to be executed as a task */
+  p->request = NONE;
+  /*----END of NEW CODE----*/
+  p->state = READY;
 
-   /*----END of NEW CODE----*/
 
-   p->state = READY;
-
+  /* ---- Need to add switch statement for handling ---- 
+   * ---- PERIODIC | SYSTEM | RR                    ----
+   */
 }
 
 
@@ -164,10 +158,8 @@ void Kernel_Create_Task_At( PD *p, voidfuncptr f )
   */
 static void Kernel_Create_Task( voidfuncptr f ) 
 {
-   int x;
-
-   if (Tasks == MAXTHREAD) return;  /* Too many task! */
-
+  int x;
+  if (Tasks == MAXTHREAD) return;  /* Too many task! */
    /* find a DEAD PD that we can use  */
   for (x = 0; x < MAXTHREAD; x++) {
     if (Process[x].state == DEAD) 
@@ -177,16 +169,16 @@ static void Kernel_Create_Task( voidfuncptr f )
   Kernel_Create_Task_At( &(Process[x]), f );
 }
 
-
 /**
   * This internal kernel function is a part of the "scheduler". It chooses the 
   * next task to run, i.e., Cp.
   */
+
 static void Dispatch()
 {
-     /* find the next READY task
-       * Note: if there is no READY task, then this will loop forever!.
-       */
+  /* find the next READY task
+   * Note: if there is no READY task, then this will loop forever!.
+   */
    while(Process[NextP].state != READY) {
       NextP = (NextP + 1) % MAXTHREAD;
    }
@@ -208,42 +200,55 @@ static void Dispatch()
   */
 static void Next_Kernel_Request() 
 {
-   Dispatch();  /* select a new task to run */
+  Dispatch();  /* select a new task to run */
 
-   while(1) {
-       Cp->request = NONE; /* clear its request */
+  while(1) {
+    Cp->request = NONE; /* clear its request */
 
-       /* activate this newly selected task */
-       CurrentSp = Cp->sp;
-       Exit_Kernel();    /* or CSwitch() */
+     /* activate this newly selected task */
+    CurrentSp = Cp->sp;
+    Exit_Kernel();    /* or CSwitch() */
 
-       /* if this task makes a system call, it will return to here! */
+    /* if this task makes a system call, it will return to here! */
 
-        /* save the Cp's stack pointer */
-       Cp->sp = CurrentSp;
+    /* save the Cp's stack pointer */
+    Cp->sp = CurrentSp;
 
-       switch(Cp->request){
-       case CREATE:
-           Kernel_Create_Task( Cp->code );
-           break;
-       case NEXT:
-	   case NONE:
-           /* NONE could be caused by a timer interrupt */
-          Cp->state = READY;
-          Dispatch();
-          break;
-       case TERMINATE:
-          /* deallocate all resources used by this task */
-          Cp->state = DEAD;
-          Dispatch();
-          break;
-       default:
-          /* Houston! we have a problem here! */
-          break;
-       }
-    } 
+    kernel_handle_request();
+  } 
 }
 
+/* 
+ * fn kernel_handle_request() 
+ * the switch statement below should be the main logic 
+ * for handling the different types of kernel_requests 
+ *
+ * Also the first part of the scheduler
+ * Determines whether current process should be
+ * in the ready or waiting queue
+*/
+static void kernel_handle_request(void)
+{
+  switch(Cp->request){
+    case CREATE:
+      Kernel_Create_Task( Cp->code );
+      break;
+    case NEXT:
+    case NONE:
+     /* NONE could be caused by a timer interrupt */
+      Cp->state = READY;
+      Dispatch();
+      break;
+    case TERMINATE:
+      /* deallocate all resources used by this task */
+      Cp->state = DEAD;
+      Dispatch();
+      break;
+    default:
+      /* Houston! we have a problem here! */
+    break;
+  }
+}
 
 /*================
   * RTOS  API  and Stubs
@@ -256,16 +261,16 @@ static void Next_Kernel_Request()
   */
 void OS_Init() 
 {
-   int x;
+  int x;
 
-   Tasks = 0;
-   KernelActive = 0;
-   NextP = 0;
+  Tasks = 0;
+  KernelActive = 0;
+  NextP = 0;
 	//Reminder: Clear the memory for the task on creation.
-   for (x = 0; x < MAXTHREAD; x++) {
-      memset(&(Process[x]),0,sizeof(PD));
-      Process[x].state = DEAD;
-   }
+  for (x = 0; x < MAXTHREAD; x++) {
+    memset(&(Process[x]),0,sizeof(PD));
+    Process[x].state = DEAD;
+  }
 }
 
 
@@ -274,15 +279,14 @@ void OS_Init()
   */
 void OS_Start() 
 {   
-   if ( (! KernelActive) && (Tasks > 0)) {
-      Disable_Interrupt();
-      /* we may have to initialize the interrupt vector for Enter_Kernel() here. */
-
-      /* here we go...  */
-      KernelActive = 1;
-      Next_Kernel_Request();
-      /* NEVER RETURNS!!! */
-   }
+  if ( (! KernelActive) && (Tasks > 0)) {
+    Disable_Interrupt();
+    /* we may have to initialize the interrupt vector for Enter_Kernel() here. */
+    /* here we go...  */
+    KernelActive = 1;
+    Next_Kernel_Request();
+    /* NEVER RETURNS!!! */
+  }
 }
 
 
@@ -293,15 +297,15 @@ void OS_Start()
   */
 void Task_Create( voidfuncptr f)
 {
-   if (KernelActive ) {
-     Disable_Interrupt();
-     Cp ->request = CREATE;
-     Cp->code = f;
-     Enter_Kernel();
-   } else { 
-      /* call the RTOS function directly */
-      Kernel_Create_Task( f );
-   }
+  if (KernelActive ) {
+    Disable_Interrupt();
+    Cp ->request = CREATE;
+    Cp->code = f;
+    Enter_Kernel();
+  } else { 
+    /* call the RTOS function directly */
+    Kernel_Create_Task( f );
+  }
 }
 
 /**
@@ -309,37 +313,35 @@ void Task_Create( voidfuncptr f)
   */
 void Task_Next() 
 {
-   if (KernelActive) {
-     Disable_Interrupt();
-     Cp ->request = NEXT;
-     Enter_Kernel();
+  if (KernelActive) {
+    Disable_Interrupt();
+    Cp ->request = NEXT;
+    Enter_Kernel();
   }
 }
-
 
 /**
   * The calling task terminates itself.
   */
 void Task_Terminate() 
 {
-   if (KernelActive) {
-      Disable_Interrupt();
-      Cp -> request = TERMINATE;
-      Enter_Kernel();
-     /* never returns here! */
-   }
+  if (KernelActive) {
+    Disable_Interrupt();
+    Cp -> request = TERMINATE;
+    Enter_Kernel();
+    /* never returns here! */
+  }
 }
-
 
 /**
   * Interrupt service routine
   */
-ISR(TIMER3_COMPA_vect)
-{
-  enable_LED(LED_ISR);
-  Task_Next();
-  disable_LEDs();
-}
+// ISR(TIMER3_COMPA_vect)
+// {
+//   enable_LED(LED_ISR);
+//   Task_Next();
+//   disable_LEDs();
+// }
 
 /**
   * This function creates two cooperative tasks, "Ping" and "Pong". Both
@@ -348,9 +350,8 @@ ISR(TIMER3_COMPA_vect)
 void main() 
 {
   OS_Init();
-  // setup();  
-   // Task_Create( Pong );
-   Task_Create( a_main );
+  // Task_Create( Pong );
+  Task_Create( a_main );
   OS_Start();
 }
 
